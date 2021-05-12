58cafee1c8a7a686ac543e067dce3e7580b630fd
Shakir Naeem <shakir.naeem@synergy-it.com>
2021-05-12 20:23:42 +0500

    migration changes

--- pages/api/brands/GetBrands.js
+++ pages/api/brands/GetBrands.js
@@ -3,11 +3,25 @@ const { PrismaClient } = require('@prisma/client')
 export default async function handler(req, res) {
     const prisma = new PrismaClient()
 
-    const brands = await prisma.brand.findMany({
-        select: {
-            id: true,
-            name: true
+    const resData = await fetch(`https://service.pakmobilezone.com/api/Brands`)
+    const dataAll = await resData.json()
+    const data = dataAll.data
+    if (dataAll) {
+        if (data.length > 0) {
+            for (var i = 0; i < data.length; i++) {
+                const user = await prisma.brand.create({
+                    data: {
+                        id: data[i].id,
+                        name: data[i].name,
+                        position: data[i].position,
+                        isDeleted: data[i].isDeleted,
+                        isPopular: data[i].isPopular
+                    },
+                })
+            }
         }
-    })
-    res.status(200).json(brands);
+    }
+
+    const brands = await prisma.brand.findMany()
+    res.status(200).json(data);
 }
