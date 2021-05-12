37f3182e181976d0d93fa6106d076c71c8f79604
Shakir Naeem <shakir.naeem@synergy-it.com>
2021-05-12 20:24:42 +0500

    brand changes

--- pages/api/brands/GetBrands.js
+++ pages/api/brands/GetBrands.js
@@ -3,25 +3,25 @@ const { PrismaClient } = require('@prisma/client')
 export default async function handler(req, res) {
     const prisma = new PrismaClient()
 
-    const resData = await fetch(`https://service.pakmobilezone.com/api/Brands`)
-    const dataAll = await resData.json()
-    const data = dataAll.data
-    if (dataAll) {
-        if (data.length > 0) {
-            for (var i = 0; i < data.length; i++) {
-                const user = await prisma.brand.create({
-                    data: {
-                        id: data[i].id,
-                        name: data[i].name,
-                        position: data[i].position,
-                        isDeleted: data[i].isDeleted,
-                        isPopular: data[i].isPopular
-                    },
-                })
-            }
-        }
-    }
+    // const resData = await fetch(`https://service.pakmobilezone.com/api/Brands`)
+    // const dataAll = await resData.json()
+    // const data = dataAll.data
+    // if (dataAll) {
+    //     if (data.length > 0) {
+    //         for (var i = 0; i < data.length; i++) {
+    //             const user = await prisma.brand.create({
+    //                 data: {
+    //                     id: data[i].id,
+    //                     name: data[i].name,
+    //                     position: data[i].position,
+    //                     isDeleted: data[i].isDeleted,
+    //                     isPopular: data[i].isPopular
+    //                 },
+    //             })
+    //         }
+    //     }
+    // }
 
     const brands = await prisma.brand.findMany()
-    res.status(200).json(data);
+    res.status(200).json(brands);
 }
