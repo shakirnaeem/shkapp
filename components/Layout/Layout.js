0b3ceda131e4492200670b3e065161c50a504a2c
Shakir Naeem <shakir.naeem@synergy-it.com>
2021-05-27 06:55:11 +0500

    test

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -4,7 +4,7 @@ import SideNav from '../navs/side-nav.js'
 export default function Layout({ children, ...pageProps }) {
     return <>
         <TopNav></TopNav>
-        <SideNav brands={pageProps.brands}></SideNav>
+        {/* <SideNav brands={pageProps.brands}></SideNav> */}
         {children}
     </>
 }
