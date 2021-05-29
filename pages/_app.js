ae0003e8d92230d41c2201148f49935e818c1e4c
Shakir Naeem <shakir.naeem@synergy-it.com>
2021-05-29 07:46:07 +0500

    s

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles.css'
+import App from "next/app"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+
+MyApp.getInitialProps = async (appContext) => {
+    // calls page's `getInitialProps` and fills `appProps.pageProps`
+    const appProps = await App.getInitialProps(appContext);
+    const res = await fetch(`https://shkapp.vercel.app/api/brands/GetBrands`)
+    const brands = await res.json();
+    appProps.pageProps = { brands }
+    return { ...appProps }
 }
 
 export default MyApp
