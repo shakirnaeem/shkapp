30e3435dbb2a0747aca5172205567daf23b5f0d9
Shakir Naeem <shakir.naeem@synergy-it.com>
2021-06-27 11:33:43 +0500

    set home link

--- components/navs/top-nav.js
+++ components/navs/top-nav.js
@@ -1,16 +1,34 @@
-export default function TopNav(props) {
+import { useRouter } from 'next/router';
 
+export default function TopNav(props) {
+    const router = useRouter()
     function toggle() {
         props.setToggleSideNav(!props.toggleSideNav)
+    }
+
+    function gotoHome() {
+        router.push('/')
     }
 
     return <nav className="navbar navbar-default fixed-top pt-0">
         <div className="container-fluid app-nav-bar rounded pt-2 pb-2">
             <div className="navbar-header">
                 <span className="d-lg-none d-md-none nav-menu text-white mr-2" onClick={toggle}>☰</span>
-                <a className="navbar-brand text-white" href="index.html">
+                <a className="navbar-brand text-white cursor-pointer" onClick={x => gotoHome()}>
                     PAKMOBILEZONE
-                    </a>
+                </a>
+                {/* <ReactSearchAutocomplete
+                    placeholder="What are you looking for?"
+                    items={devices}
+                    //onSearch={handleOnSearch}
+                    //onSelect={handleOnSelect}
+                    styling={{
+                        height: '35px',
+                        zIndex: 10000
+                    }}
+                    //onFocus={handleOnFocus}
+                    autoFocus
+                /> */}
             </div>
         </div>
     </nav>
